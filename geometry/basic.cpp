036d6eab33270a3c04d92d6ebd8fb68c38ab9a38
Lê Mạnh Khương <65122571+KhuongLe329@users.noreply.github.com>
2020-08-11 01:16:43 +0700

    Create basic.cpp

---
+++ geometry/basic.cpp
@@ -0,0 +1,210 @@
+/// copy from ngthanhtrung23
+
+#include<bits/stdc++.h>
+#define ll            long long
+#define pb            emplace_back
+#define fi            first
+#define se            second
+#define mp            make_pair
+#define int           int64_t
+
+using namespace std;
+
+typedef pair<int, int> pii;
+const int N = int(1e5) + 1;
+
+#define EPS 1e-6
+const double PI = acos(-1.0);
+
+double DEG_to_RAD(double d) { return d * PI / 180.0; }
+double RAD_to_DEG(double r) { return r * 180.0 / PI; }
+
+inline int cmp(double a, double b) {
+    return (a < b - EPS) ? -1 : ((a > b + EPS) ? 1 : 0);
+}
+
+struct Point {
+    double x, y;
+    Point() { x = y = 0.0; }
+    Point(double x, double y) : x(x), y(y) {}
+
+    Point operator + (const Point& a) const { return Point(x+a.x, y+a.y); }
+    Point operator - (const Point& a) const { return Point(x-a.x, y-a.y); }
+    Point operator * (double k) const { return Point(x*k, y*k); }
+    Point operator / (double k) const { return Point(x/k, y/k); }
+
+    double operator * (const Point& a) const { return x*a.x + y*a.y; } // dot product
+    double operator % (const Point& a) const { return x*a.y - y*a.x; } // cross product
+
+    int cmp(Point q) const { if (int t = ::cmp(x,q.x)) return t; return ::cmp(y,q.y); }
+
+    #define Comp(x) bool operator x (Point q) const { return cmp(q) x 0; }
+    Comp(>) Comp(<) Comp(==) Comp(>=) Comp(<=) Comp(!=)
+    #undef Comp
+
+    Point conj() { return Point(x, -y); }
+    double norm() { return x*x + y*y; }
+
+    // Note: There are 2 ways for implementing len():
+    // 1. sqrt(norm()) --> fast, but inaccurate (produce some values that are of order X^2)
+    // 2. hypot(x, y) --> slow, but much more accurate
+    double len() { return sqrt(norm()); }
+
+    Point rotate(double alpha) {
+        double cosa = cos(alpha), sina = sin(alpha);
+        return Point(x * cosa - y * sina, x * sina + y * cosa);
+    }
+};
+typedef vector< Point > Polygon;
+int ccw(Point a, Point b, Point c) {
+    return cmp((b-a)%(c-a),0);
+}
+int RE_TRAI = ccw(Point(0, 0), Point(0, 1), Point(-1, 1));
+int RE_PHAI = ccw(Point(0, 0), Point(0, 1), Point(1, 1));
+istream& operator >> (istream& cin, Point& p) {
+    cin >> p.x >> p.y;
+    return cin;
+}
+ostream& operator << (ostream& cout, Point& p) {
+    cout << p.x << ' ' << p.y;
+    return cout;
+}
+
+double angle(Point a, Point o, Point b) { // min of directed angle AOB & BOA
+    a = a - o; b = b - o;
+    return acos((a * b) / sqrt(a.norm()) / sqrt(b.norm()));
+}
+
+struct Line {
+    double a, b, c;
+    Point A, B; // Added for polygon intersect line. Do not rely on assumption that these are valid
+
+    Line(double a, double b, double c) : a(a), b(b), c(c) {}
+
+    Line(Point A, Point B) : A(A), B(B) {
+        a = B.y - A.y;
+        b = A.x - B.x;
+        c = - (a * A.x + b * A.y);
+    }
+    Line(Point P, double m) {
+        a = -m; b = 1;
+        c = -((a * P.x) + (b * P.y));
+    }
+    double f(Point A) {
+        return a*A.x + b*A.y + c;
+    }
+};
+
+bool areParallel(Line l1, Line l2) {
+    return cmp(l1.a*l2.b, l1.b*l2.a) == 0;
+}
+
+bool areSame(Line l1, Line l2) {
+    return areParallel(l1 ,l2) && cmp(l1.c*l2.a, l2.c*l1.a) == 0
+                && cmp(l1.c*l2.b, l1.b*l2.c) == 0;
+}
+
+bool areIntersect(Line l1, Line l2, Point &p) {
+    if (areParallel(l1, l2)) return false;
+    double dx = l1.b*l2.c - l2.b*l1.c;
+    double dy = l1.c*l2.a - l2.c*l1.a;
+    double d  = l1.a*l2.b - l2.a*l1.b;
+    p = Point(dx/d, dy/d);
+    return true;
+}
+double area2(Point a, Point b, Point c) { return a%b + b%c + c%a; }
+
+void ConvexHull(vector<Point> &pts) {
+    sort(pts.begin(), pts.end());
+    pts.erase(unique(pts.begin(), pts.end()), pts.end());
+    vector<Point> up, dn;
+    for (int i = 0; i < pts.size(); i++) {
+        // Note: If need maximum points on convex hull, need to change >= and <= to > and <.
+        while (up.size() > 1 && area2(up[up.size()-2], up.back(), pts[i]) >= 0) up.pop_back();
+        while (dn.size() > 1 && area2(dn[dn.size()-2], dn.back(), pts[i]) <= 0) dn.pop_back();
+        up.push_back(pts[i]);
+        dn.push_back(pts[i]);
+    }
+    pts = dn;
+    for (int i = (int) up.size() - 2; i >= 1; i--) pts.push_back(up[i]);
+}
+
+double signed_area(Polygon p) {
+    double area = 0;
+    for(int i = 0; i < p.size(); i++) {
+        int j = (i+1) % p.size();
+        area += p[i].x*p[j].y - p[j].x*p[i].y;
+    }
+    return area / 2.0;
+}
+double area(const Polygon &p) {
+    return fabs(signed_area(p));
+}
+// Cut a polygon with a line. Returns one half.
+// To return the other half, reverse the direction of Line l (by negating l.a, l.b)
+// The line must be formed using 2 points
+Polygon polygon_cut(const Polygon& P, Line l) {
+    Polygon Q;
+    for(int i = 0; i < P.size(); ++i) {
+        Point A = P[i], B = (i == P.size()-1) ? P[0] : P[i+1];
+        if (ccw(l.A, l.B, A) != -1) Q.push_back(A);
+        if (ccw(l.A, l.B, A)*ccw(l.A, l.B, B) < 0) {
+            Point p; areIntersect(Line(A, B), l, p);
+            Q.push_back(p);
+        }
+    }
+    return Q;
+}
+
+typedef long double ld;
+int n, k, u, v, sz;
+double ax;
+Polygon p, q;
+Point a, b, c;
+vector<Polygon> vp, cp;
+
+Point FindPoint(Point a, Point b, ld ang) {
+     Point c;
+     a.x = b.x * 2 - a.x;
+     a.y = b.y * 2 - a.y;
+     /// quay vector a theo b 1 goc Pi - ang
+     a.x -= b.x, a.y -= b.y;
+     c.x = a.x * cos(ang) - a.y * sin(ang) + b.x;
+     c.y = a.x * sin(ang) + a.y * cos(ang) + b.y;
+     return c;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+#define FileName      "cake"
+    if(fopen(FileName".inp", "r")) {
+       freopen(FileName".inp", "r", stdin);
+       freopen(FileName".out", "w", stdout);
+    }
+    cin >> n; p.clear();
+    a = Point(0, 0); ax = DEG_to_RAD(180.0 - (n - 2) * 180.0 /(ld) n);
+    p.push_back(a); b = Point(50, 0); p.push_back(b);
+    while(p.size() < n) {
+        c = FindPoint(a, b, ax);
+        p.push_back(c);
+        a = b, b = c;
+    }
+    cin >> k; vp.push_back(p);
+    cout << fixed << setprecision(4);
+    for(int i = 1; i <= k; ++i) {
+        cin >> u >> v; --u, --v;
+        if(u == v) continue;
+        Line l = Line(p[u], p[v]);
+        Line nl = Line(p[v], p[u]);
+        cp.clear();
+        for(auto p: vp) {
+            q = polygon_cut(p, l);
+            if(q.size() > 2) cp.push_back(q);
+            q = polygon_cut(p, nl);
+            if(q.size() > 2) cp.push_back(q);
+        }
+        vp.clear(); vp = cp;
+    }
+    cout << vp.size();
+}
