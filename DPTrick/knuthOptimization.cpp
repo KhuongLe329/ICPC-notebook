0a8e4df24ef8066a4434829b8198785bc9c6e6cf
Lê Mạnh Khương <65122571+KhuongLe329@users.noreply.github.com>
2022-10-02 17:36:40 +0900

    Update knuthOptimization.cpp

--- DPTrick/knuthOptimization.cpp
+++ DPTrick/knuthOptimization.cpp
@@ -1,60 +1,18 @@
-/*
-  OPTCUT spoj
-*/
-#include<bits/stdc++.h>
-#define For(i, a, b)  for(int i = a; i <= b; ++i)
-#define Ford(i, a, b) for(int i = a; i >= b; --i)
-#define ll            long long
-#define pb            push_back
- 
-const int N = 2e3 + 7;
- 
-using namespace std;
- 
-int n, m, a[N], pos[N][N], dp[N][N], c[N][N];
- 
-void Init()
-{
-    cin >> n;
-    For(i, 1, n)
-    {
-        cin >> a[i];
-        a[i] += a[i - 1];
-    }
-    For(i, 1, n)
-    For(j, i, n)
-    c[i][j] = a[j] - a[i - 1];
-}
- 
-void Solve()
-{
-     fill_n(&dp[0][0], N * N, 1e9 + 1);
-     For(i, 1, n) dp[i][i] = 0, pos[i][i] = i;
-     For(len, 2, n)
-     {
-         For(i, 1, n - len + 1)
-         {
-             int j = i + len - 1;
-             For(k, pos[i][j - 1], pos[i + 1][j])
-             {
-                 m = c[i][j] + dp[i][k - 1] + dp[k][j];
-                 if(m < dp[i][j])
-                 {
-                     dp[i][j] = m;
-                     pos[i][j] = k;
-                 }
-             }
-         }
-     }
-     cout << dp[1][n];
-}
- 
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    //READ; WRITE;
-    Init();
-    Solve();
-}
+// Original Recurrence:
+//   dp[i][j] = min(dp[i][k] + dp[k][j]) + C[i][j]   for k = i+1..j-1
+// Necessary & Sufficient Conditions:
+//   A[i][j-1] <= A[i][j] <= A[i+1][j]
+//   with A[i][j] = smallest k that gives optimal answer
+// Also applicable if the following conditions are met:
+//   1. C[a][c] + C[b][d] <= C[a][d] + C[b][c] (quadrangle inequality)
+//   2. C[b][c] <= C[a][d]                     (monotonicity)
+//   for all a <= b <= c <= d
+// To use:
+//   Calculate dp[i][i] and A[i][i]
+//
+//   FOR(len = 1..n-1)
+//     FOR(i = 1..n-len) {
+//       j = i + len
+//       FOR(k = A[i][j-1]..A[i+1][j])
+//         update(dp[i][j])
+//     }
