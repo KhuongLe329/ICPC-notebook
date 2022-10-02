b616ec3611c197af35ed1c51aaaa1754fbf1c0d0
Lê Mạnh Khương <65122571+KhuongLe329@users.noreply.github.com>
2022-10-02 17:21:39 +0900

    Update maxflowEdmondKarp.cpp

--- graph/maxflowEdmondKarp.cpp
+++ graph/maxflowEdmondKarp.cpp
@@ -1,15 +1,3 @@
-#include <bits/stdc++.h>
-#define pb          emplace_back
-#define ll          long long
-#define fi          first
-#define se          second
-#define mp          make_pair
-
-using namespace std;
-
-const int N = 102;
-typedef pair<int, int> pii;
-
 struct TFlow {
     struct TEdge {
         int u, v, cf;
@@ -57,39 +45,3 @@ struct TFlow {
         return FlowVal;
     }
 };
-
-int n, m;
-string s;
-pii S, T;
-
-int32_t main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0), cout.tie(0);
-    #define Task        "test"
-    if(fopen(Task".inp", "r")) {
-        freopen(Task".inp", "r", stdin);
-        freopen(Task".out", "w", stdout);
-    }
-    cin >> n >> m;
-    TFlow Flow = TFlow(0, n + m + 1, n + m + 2);
-    ///s = 0, t = n + m + 1, n row(1 -> n), m col(n + 1 -> n + m)
-    for(int i = 1; i <= n; ++i) {
-        cin >> s; s = ' ' + s;
-        for(int j = 1; j <= m; ++j) {
-            if(s[j] == '.') continue;
-            if(s[j] == 'S') {
-                Flow.AddEdge(0, i, int(1e9 + 7));
-                Flow.AddEdge(0, j + n, int(1e9 + 7));
-                S = mp(i, j);
-            } else if(s[j] == 'T') {
-                Flow.AddEdge(j + n, n + m + 1, int(1e9 + 7));
-                Flow.AddEdge(i, n + m + 1, int(1e9 + 7));
-                T = mp(i, j);
-            }
-            Flow.AddEdge(i, j + n, 1);
-            Flow.AddEdge(j + n, i, 1);
-        }
-    }
-    if(S.fi == T.fi || S.se == T.se) return cout << -1, 0;
-    else cout << Flow.GetMaxFlow();
-}
